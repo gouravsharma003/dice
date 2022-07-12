9ca2b744c7e038c0f5f3c8e6875246bad9930846
gouravsharma003 <108124208+gouravsharma003@users.noreply.github.com>
2022-07-12 14:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomDiceImage="Dice"+randomNumber1+".png";
+var randomDiceImage="dice"+randomNumber1+".png";
 var randomImageSource="./"+randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="./Dice"+randomNumber2+".png";
+var randomImageSource2="./dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 if (randomNumber1 > randomNumber2){
